80628e08f90e67c93e197ef6dc39695bac9c40fd
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-22 15:15:07 +0800

    Update InputGradeTable.vue

--- src/views/components/InputGradeTable.vue
+++ src/views/components/InputGradeTable.vue
@@ -344,3 +344,220 @@ export default {
   },
 
   methods: {
+    
+    // 获取年份下拉框选项
+    getYearOptions() {
+      // 加载年份下拉选项
+      settingService.getTeacherYearList().then(response => {
+        if (response.code === 200) {
+          this.formOptions.items[0].itemRender.options = response.data;
+          this.gridOptions.formConfig.items[0].itemRender.options = response.data;
+
+          if (response.data[0] != null) {
+            const year = response.data[0].value;
+            this.formOptions.data = {year: year}
+            const yearRow = {};
+            yearRow.value = year;
+            this.inputYearChange(null, yearRow);
+            this.queryYearChange(null, yearRow);
+            this.setExamMessage();
+
+          }
+        }
+      });
+    },
+
+    setExamMessage() {
+      const searchData = this.gridOptions.formConfig.data;
+      settingService.getExamMessage(JSON.stringify(searchData)).then(response => {
+        if (response.code === 200) {
+          this.examMessage = response.data;
+        }
+      });
+    },
+
+    // 新增时，年份变化下拉框联动
+    inputYearChange(row, yearRow) {
+      const year = yearRow.value;
+      if (year != null && year !== '') {
+        // 加载班级下拉选项
+        settingService.getTeacherClassListByCondition({year: year}).then(response => {
+          if (response.code === 200) {
+            this.formOptions.data = {year: year, classId: ''}
+            this.formOptions.items[1].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const classId = response.data[0].value;
+              this.formOptions.data = {year: year, classId: classId}
+              const classRow = {};
+              classRow.value = classId;
+              this.inputClassChange(null, classRow);
+            }
+          }
+        });
+      }
+    },
+    // 新增时，班级变化下拉框联动
+    inputClassChange(row, classRow) {
+      const classId = classRow.value;
+      const year = this.formOptions.data.year;
+      if (classId != null && classId !== '') {
+        // 加载学期下拉选项
+        settingService.getTeacherSemesterListByCondition({year: year, classId: classId}).then(response => {
+          if (response.code === 200) {
+            this.formOptions.items[2].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const semesterId = response.data[0].value;
+              this.formOptions.data = {year: year, classId: classId, semesterId: semesterId}
+              const semesterRow = {};
+              semesterRow.value = semesterId;
+
+              // 加载学科下拉选项
+              this.inputSemesterChange(null, semesterRow);
+            }
+          }
+        });
+      }
+    },
+    // 新增时，学期变化下拉框联动
+    inputSemesterChange(row, semesterRow) {
+      const semesterId = semesterRow.value;
+      const year = this.formOptions.data.year;
+      const classId = this.formOptions.data.classId;
+      if (classId != null && classId !== '') {
+        settingService.getTeacherSubjectListByCondition({
+          year: year,
+          classId: classId,
+          semesterId: semesterId
+        }).then(response => {
+          if (response.code === 200) {
+            this.formOptions.items[3].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const subjectId = response.data[0].value;
+              this.formOptions.data = {year: year, classId: classId, semesterId: semesterId, subjectId: subjectId}
+            }
+          }
+        });
+      }
+    },
+
+    // 查询时，年份变化下拉框联动
+    queryYearChange(row, yearRow) {
+      const year = yearRow.value;
+      if (year != null && year !== '') {
+        // 加载班级下拉选项
+        settingService.getTeacherClassListByCondition({year: year}).then(response => {
+          if (response.code === 200) {
+            this.gridOptions.formConfig.data = {year: year, classId: ''}
+            this.gridOptions.formConfig.items[1].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const classId = response.data[0].value;
+              this.gridOptions.formConfig.data = {year: year, classId: classId}
+              const classRow = {};
+              classRow.value = classId;
+              this.queryClassChange(null, classRow);
+            }
+          }
+        });
+      }
+    },
+    // 查询时，班级变化下拉框联动
+    queryClassChange(row, classRow) {
+      const classId = classRow.value;
+      const year = this.gridOptions.formConfig.data.year;
+      if (classId != null && classId !== '') {
+        // 加载学期下拉选项
+        settingService.getTeacherSemesterListByCondition({year: year, classId: classId}).then(response => {
+          if (response.code === 200) {
+            this.gridOptions.formConfig.items[2].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const semesterId = response.data[0].value;
+              this.gridOptions.formConfig.data = {year: year, classId: classId, semesterId: semesterId}
+              const semesterRow = {};
+              semesterRow.value = semesterId;
+
+              // 加载学科下拉选项
+              this.querySemesterChange(null, semesterRow);
+            }
+          }
+        });
+      }
+    },
+    // 查询时，学期变化下拉框联动
+    querySemesterChange(row, semesterRow) {
+      const semesterId = semesterRow.value;
+      const year = this.gridOptions.formConfig.data.year;
+      const classId = this.gridOptions.formConfig.data.classId;
+      if (classId != null && classId !== '') {
+        settingService.getTeacherSubjectListByCondition({
+          year: year,
+          classId: classId,
+          semesterId: semesterId
+        }).then(response => {
+          if (response.code === 200) {
+            this.gridOptions.formConfig.items[3].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const subjectId = response.data[0].value;
+              this.gridOptions.formConfig.data = {
+                year: year,
+                classId: classId,
+                semesterId: semesterId,
+                subjectId: subjectId
+              }
+
+              const subjectRow = {};
+              subjectRow.value = subjectId;
+              // 加载学科下拉选项
+              this.querySubjectChange(null, subjectRow);
+            }
+          }
+        });
+      }
+    },
+    // 查询时，学科变化下拉框联动
+    querySubjectChange(row, subjectRow) {
+      const subjectId = subjectRow.value;
+      const year = this.gridOptions.formConfig.data.year;
+      const classId = this.gridOptions.formConfig.data.classId;
+      const semesterId = this.gridOptions.formConfig.data.semesterId;
+      if (classId != null && classId !== '') {
+        settingService.getTeacherExamListByCondition({
+          year: year,
+          classId: classId,
+          semesterId: semesterId,
+          subjectId: subjectId
+        }).then(response => {
+          if (response.code === 200) {
+            this.gridOptions.formConfig.items[4].itemRender.options = response.data;
+            if (response.data[0] != null) {
+              const examCode = response.data[0].value;
+              this.gridOptions.formConfig.data = {
+                year: year,
+                classId: classId,
+                semesterId: semesterId,
+                subjectId: subjectId,
+                examCode: examCode
+              }
+            }
+          }
+        });
+      }
+    },
+    // 查询时，考试名称变化重新加载table数据
+    queryExamChange() {
+      this.queryGradeInfo();
+    },
+
+    // 表头搜索
+    confirmFilterEvent(option) {
+      const $grid = this.$refs.gridRef
+      if ($grid) {
+        // 设置为选中状态
+        $grid.updateFilterOptionStatus(option, !!option.data)
+        // 修改条件之后，需要手动调用 updateData 处理表格数据
+        $grid.updateData()
+      }
+    },
+
+
+ 
+
