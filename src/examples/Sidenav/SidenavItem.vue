816c897f9e37d691a624b5c26e49358a880fa495
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-24 16:03:22 +0800

    Update SidenavItem.vue

--- src/examples/Sidenav/SidenavItem.vue
+++ src/examples/Sidenav/SidenavItem.vue
@@ -1,83 +1,37 @@
 <script setup>
-import { computed } from "vue";
+
 import { useStore } from "vuex";
+
 const store = useStore();
 
-const isRTL = computed(() => store.state.isRTL);
-const layout = computed(() => store.state.layout);
+const sidebarMinimize = () => store.commit("sidebarMinimize");
+
+const minimizeSidebar = () => {
+  if (window.innerWidth < 1200) {
+    sidebarMinimize();
+  }
+};
+
 defineProps({
-  card: {
-    type: Object,
+  to: {
+    type: String,
     required: true,
-    title: String,
-    description: String,
-    links: {
-      type: Array,
-      label: String,
-      route: String,
-      color: String,
-    },
+  },
+  navText: {
+    type: String,
+    required: true,
   },
 });
 </script>
 <template>
-  <div
-    v-show="layout !== 'landing'"
-    class="card card-plain shadow-none"
-    id="sidenavCard"
-  >
-    <div class="p-3 card-body text-center w-100 pt-0">
-      <img
-        class="w-50 mx-auto"
-        src="@/assets/img/illustrations/icon-documentation.svg"
-        alt="sidebar_illustration"
-      />
-
-      <h6 v-if="isRTL" class="mb-0 text-dark up">تحتاج مساعدة ؟</h6>
-
-      <h6 v-else class="mb-0 text-dark up">Need Help ?</h6>
-
-      <p v-if="isRTL" class="text-xs font-weight-bold">
-        يرجى التحقق من مستنداتنا
-      </p>
-
-      <p v-else class="text-xs font-weight-bold">Please check our docs</p>
+  <router-link :to="to" class="nav-link" @click="minimizeSidebar">
+    <div
+      class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center"
+    >
+      <slot name="icon"></slot>
     </div>
-
-    <a
-      v-if="isRTL"
-      href="https://www.creative-tim.com/learning-lab/vue/overview/argon-dashboard/"
-      target="_blank"
-      class="mb-3 btn btn-dark btn-sm w-100"
-    >
-      توثيق
-    </a>
-
-    <a
-      v-else
-      href="https://www.creative-tim.com/learning-lab/vue/overview/argon-dashboard/"
-      target="_blank"
-      class="mb-3 btn btn-dark btn-sm w-100"
-    >
-      Documentation
-    </a>
-
-    <a
-      v-if="isRTL"
-      href="https://www.creative-tim.com/product/vue-argon-dashboard-pro"
-      target="_blank"
-      class="mb-3 btn btn-success btn-sm w-100"
-    >
-      التطور للاحترافية
-    </a>
-
-    <a
-      v-else
-      href="https://www.creative-tim.com/product/vue-argon-dashboard-pro"
-      target="_blank"
-      class="mb-3 btn btn-success btn-sm w-100"
-    >
-      Upgrade to pro
-    </a>
-  </div>
+    <span class="nav-link-text ms-1 pt-1">{{
+      navText
+    }}</span>
+  </router-link>
 </template>
