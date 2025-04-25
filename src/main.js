ebe3ee58beb24f890e7cea543bc4744a32f04920
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-25 17:09:29 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,38 +5,40 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
-// import * as auth from './utils/auth'
 import Message from './utils/message.js';
+import { isTokenExpired, getToken, removeToken } from './utils/auth';
 
 import VxeUI from 'vxe-pc-ui'
 import 'vxe-pc-ui/lib/style.css'
 import VxeUITable from 'vxe-table'
 import 'vxe-table/lib/style.css'
+import VxeUIPluginExportXLSX from '@vxe-ui/plugin-export-xlsx'
+import ExcelJS from 'exceljs'
 
+VxeUI.use(VxeUIPluginExportXLSX, {
+    ExcelJS
+})
 const appInstance = createApp(App);
 
-
-//这个方法需要放在new Vue之前，不然按F5刷新页面不会调用这个方法
-router.beforeEach(function (to, from, next) {
-    const token = localStorage.getItem('token');
-    if (to.matched.some(record => record.meta.needLogin) && !token) {
-        next('/login');
+router.beforeEach((to, from, next) => {
+    const token = getToken();
+    const needLogin = to.matched.some(record => record.meta.needLogin);
+    console.log("needLogin :" +needLogin);
+    console.log("!token :" +!token);
+    console.log("isTokenExpired(token) :" + isTokenExpired(token));
+    if (needLogin) {
+        if (!token || isTokenExpired(token)) {
+            removeToken();
+            next({
+                path: '/login',
+            });
+        } else {
+            next();
+        }
     } else {
         next();
     }
-    // if (to.meta.needLogin) {
-    //     if (auth.getAdminInfo()) {
-    //         console.log(auth.getAdminInfo())
-    //         next();
-    //     }else {
-    //         next({
-    //             path:'/login'
-    //         });
-    //     }
-    // }else{
-    //     next();
-    // }
-})
+});
 
 
 appInstance.use(VxeUI).use(VxeUITable);
