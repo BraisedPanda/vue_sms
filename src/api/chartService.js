cac8a0940ca8b7b3d5754e10d9bf8047a081b180
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-25 16:52:50 +0800

    Update chartService.js

--- src/api/chartService.js
+++ src/api/chartService.js
@@ -9,4 +9,16 @@ export function getStudentChart2Data(params) {
     return http.post("/chart/getStudentChart2Data", params);
 }
 
+export function getClassChart1Data(params) {
+    return http.post("/chart/getClassChart1Data", params);
+}
+
+export function getClassChart2Data(params) {
+    return http.post("/chart/getClassChart2Data", params);
+}
+
+export function getClassChart3Data(params) {
+    return http.post("/chart/getClassChart3Data", params);
+}
+
 
