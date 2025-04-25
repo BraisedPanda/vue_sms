237d33f9d094ff90594c255e11e3585c488da93c
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-25 17:13:45 +0800

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,39 @@
-import Cookies from 'js-cookie'
-const adminInfo = "user"
-
-//获取用户信息
-export function getAdminInfo() {
-    // const admin = Cookies.get(adminInfo)
-    const admin = localStorage.getItem(adminInfo);
-    if(admin){
-        return JSON.parse(admin)
+export function isTokenExpired(token) {
+    if (!token) {
+        return true;
     }
-    return ''
+    try {
+        const parsedToken = parseJwt(token);
+        const exp = parsedToken ? parsedToken.exp : 0;
+        const isTokenExpired = exp * 1000 < Date.now();
+        return isTokenExpired;
+    } catch (error) {
+        console.log("error： " + error.toString())
+        return true;
+    }
 }
-//存储用户信息
-export function setAdminInfo(admin) {
-    return Cookies.set(adminInfo, JSON.stringify(admin))
-}
-//移除用户信息
-export function removeAdminInfo() {
 
-    return Cookies.remove(adminInfo)
+export function getToken() {
+    return localStorage.getItem('token');
+}
+
+export function setToken(token) {
+    localStorage.setItem('token', token);
+}
+
+export function removeToken() {
+    localStorage.removeItem('token');
+}
+
+function parseJwt(token) {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+    } catch (e) {
+        return null;
+    }
 }
