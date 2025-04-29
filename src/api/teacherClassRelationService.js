31c7cfad15186607569d7b86e0cf44ddb7b1c1ca
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-04-29 16:01:07 +0800

    Update teacherClassRelationService.js

--- src/api/teacherClassRelationService.js
+++ src/api/teacherClassRelationService.js
@@ -5,5 +5,9 @@ export function createTeacherClassRelation(params) {
     return http.post("/teacherClassRelation/create", params);
 }
 
+export function deleteByObid(params) {
+    return http.delete("/teacherClassRelation/deleteByObid", params);
+}
+
 
 
