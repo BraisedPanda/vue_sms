f0b879060ad73b0e0e6c865d95d31949f7d9544e
BraisedPanda <51229676+BraisedPanda@users.noreply.github.com>
2025-05-14 16:31:53 +0800

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,45 +1,8 @@
 <script setup>
-import MiniStatisticsCard from "@/examples/Cards/MiniStatisticsCard.vue";
 import MiniImgCard from "@/examples/Cards/MiniImgCard.vue";
 import * as echarts from 'echarts';
 import Carousel from "./components/Carousel.vue";
-import CategoriesList from "./components/CategoriesList.vue";
-
-import US from "@/assets/img/icons/flags/US.png";
-import DE from "@/assets/img/icons/flags/DE.png";
-import GB from "@/assets/img/icons/flags/GB.png";
-import BR from "@/assets/img/icons/flags/BR.png";
-
-const sales = {
-  us: {
-    country: "United States",
-    sales: 2500,
-    value: "$230,900",
-    bounce: "29.9%",
-    flag: US,
-  },
-  germany: {
-    country: "Germany",
-    sales: "3.900",
-    value: "$440,000",
-    bounce: "40.22%",
-    flag: DE,
-  },
-  britain: {
-    country: "Great Britain",
-    sales: "1.400",
-    value: "$190,700",
-    bounce: "23.44%",
-    flag: GB,
-  },
-  brasil: {
-    country: "Brasil",
-    sales: "562",
-    value: "$143,960",
-    bounce: "32.14%",
-    flag: BR,
-  },
-};
+// import CategoriesList from "./components/CategoriesList.vue";
 
 
 
@@ -48,6 +11,9 @@ const sales = {
 <script>
 
 import * as chartService from "@/api/chartService";
+import axios from "axios";
+import { getLunar } from "chinese-lunar-calendar";
+import * as settingService from "@/api/settingInfoService";
 
 export default {
 
@@ -55,30 +21,130 @@ export default {
 
     const char1xData = [];
     const char1yData = [];
+    const currentTime = '';
+    const curYear = null; // 当前年份
+    const curMonth = ""; // 当前月份
+    const curDay = ""; // 当前日期
+    const curWeek = ""; // 当前周
+    const lunarYear = "";
+    const lunarDay = ""; // 农历日期
+    const dayOfYear = ""; // 当前日期是一年中的第几天
+    const weekOfYear = ""; // 当前日期是一年的第几周
+    const averageChange = "";
+    const moreThan90CountChange = "";
 
     return {
       char1xData,
       char1yData,
       todayWeather: {
-        cityName: '合肥',
-        today: '06月27日(星期四)',
-        maxTemp: '35℃',
-        minTemp: '24℃',
-        nowTemp: "32.6℃",
+        address: '安徽省 合肥市',
+        temp: "32.6℃",
         weather: '多云',
-        aqi: '56',
-        wind: '西南风3级',
+        windDirection: '东',
+        windPower: '<3',
+        humidity: '40%',
+        src: 'weather/weather.png',
       },
+      currentTime,
+      curYear,
+      curMonth,
+      curDay,
+      curWeek,
+      lunarYear,
+      lunarDay,
+      dayOfYear,
+      weekOfYear,
+      averageChange,
+      moreThan90CountChange,
     };
   },
   mounted() {
     this.reloadChar1Data();
-    // this.getTodayWeather();
+    this.getTodayWeather();
+    this.initDate();
   },
 
+  created() {
+    this.updateTime();
+  },
 
   methods: {
 
+    updateTime() {
+      setInterval(() => {
+        this.currentTime = new Date().toLocaleTimeString();
+      }, 1000);
+
+    },
+
+    initDate() {
+      // 初始化日历
+      const date = new Date();
+      this.curYear = date.getFullYear();
+      this.curMonth = date.getMonth() + 1;
+      this.curDay = date.getDate();
+
+      const weekday = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
+      const day = date.getDay();
+      this.curWeek = weekday[day];
+
+
+      const lunarDate = getLunar(this.curYear, this.curMonth, this.curDay);
+      this.lunarYear = lunarDate.lunarYear;
+      this.lunarDay = lunarDate.dateStr;
+      // 获取当前时间是当年的第几天
+      this.dayOfYear = Math.floor((date - new Date(this.curYear, 0, 0)) / 86400000);
+      // 获取当前时间是当年的第几周
+      const start = new Date(this.curYear, 0, 1);
+      this.weekOfYear = Math.ceil((((date - start) / 86400000) + start.getDay() + 1) / 7);
+
+      settingService.getDashboard().then(response => {
+        if (response.code === 200) {
+          this.averageChange = response.data.averageChange;
+          this.moreThan90CountChange = response.data.moreThan90CountChange;
+
+        }
+      });
+
+    },
+
+    getTodayWeather() {
+      axios.get("https://www.mxnzp.com/api/weather/current/合肥市?app_id=gpyu9yjltmjmgovk&app_secret=TpjigP4XZsp27gidYCnKZkDvMeX5zlqF").then(response => {
+        if (response.data.code === 1) {
+          const weather = response.data.data;
+          this.todayWeather.temp = weather.temp;
+          this.todayWeather.weather = weather.weather;
+          this.todayWeather.windDirection = weather.windDirection;
+          this.todayWeather.windPower = weather.windPower;
+          this.todayWeather.humidity = weather.humidity;
+          if(weather.weather.includes("晴")) {
+            this.todayWeather.src = 'weather/sun.png';
+          }
+          if(weather.weather.includes("多云")) {
+            this.todayWeather.src = 'weather/cloudy2.png';
+          }
+          if(weather.weather.includes("转晴") || weather.weather.includes("阴")) {
+            this.todayWeather.src = 'weather/cloudy.png';
+          }
+          if(weather.weather.includes("雷")) {
+            this.todayWeather.src = 'weather/flash.png';
+          }
+          if(weather.weather.includes("雾")) {
+            this.todayWeather.src = 'weather/fog.png';
+          }
+          if(weather.weather.includes("雨")) {
+            this.todayWeather.src = 'weather/rain.png';
+          }
+          if(weather.weather.includes("雪")) {
+            this.todayWeather.src = 'weather/snow.png';
+          }
+          if(weather.weather.includes("风")) {
+            this.todayWeather.src = 'weather/wind.png';
+          }
+        }
+      });
+
+    },
 
     reloadChar1Data() {
       chartService.getDefaultClassChart1Data().then(response => {
@@ -89,7 +155,6 @@ export default {
         }
       });
     },
-
 
 
     initChart1() {
@@ -199,64 +264,60 @@ export default {
     <div class="row">
       <div class="col-lg-12">
         <div class="row">
+
           <div class="col-lg-3 col-md-6 col-12">
             <mini-img-card
-              :title="todayWeather.cityName + '  ' + todayWeather.today"
-              :value="todayWeather.weather"
-              :description="todayWeather.minTemp + '~' + todayWeather.maxTemp + ' ' + todayWeather.wind"
+                :title= "curYear+'年'+curMonth+'月'+curDay+'日'"
+                :value="currentTime"
+                :description="lunarYear+lunarDay + ' ' + curWeek +'<br>'+   '第'+dayOfYear +'天 第'+weekOfYear +'周'"
+                :img="{
+                src: '/calendar.png',
+                shape: 'rounded-circle',
+              }"
+            />
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-12">
+            <mini-img-card
+              :title="todayWeather.address"
+              :value="todayWeather.weather + '  ' + todayWeather.temp"
+              :description="todayWeather.windDirection + '风' + todayWeather.windPower + '<br> 湿度' + todayWeather.humidity"
               :img="{
-                src: 'https://i.i8tq.com/e_index/todayweather/01_d.png',
+                src: todayWeather.src,
                 shape: 'rounded-circle',
               }"
             />
           </div>
 
           <div class="col-lg-3 col-md-6 col-12">
-            <mini-statistics-card
-              title="Today's Users"
-              value="2,300"
-              description="<span
-                class='text-sm font-weight-bolder text-success'
-                >+3%</span> since last week"
-              :icon="{
-                component: 'ni ni-world',
-                background: 'bg-gradient-danger',
+            <mini-img-card
+                title= "平均分变化"
+                :value="averageChange"
+                description="*<br>最近一次考试与上次考试平均分比较"
+                :img="{
+                src: '/average.png',
                 shape: 'rounded-circle',
               }"
             />
           </div>
 
           <div class="col-lg-3 col-md-6 col-12">
-            <mini-statistics-card
-              title="New Clients"
-              value="+3,462"
-              description="<span
-                class='text-sm font-weight-bolder text-danger'
-                >-2%</span> since last quarter"
-              :icon="{
-                component: 'ni ni-paper-diploma',
-                background: 'bg-gradient-success',
+            <mini-img-card
+                title="90分以上人数"
+                :value="moreThan90CountChange"
+                description="*<br>最近一次考试与上次考试90分以上人数比较"
+                :img="{
+                src: '/moreThan90.png',
                 shape: 'rounded-circle',
               }"
             />
           </div>
 
-          <div class="col-lg-3 col-md-6 col-12">
-            <mini-statistics-card
-              title="Sales"
-              value="$103,430"
-              description="<span
-                class='text-sm font-weight-bolder text-success'
-                >+5%</span> than last month"
-              :icon="{
-                component: 'ni ni-cart',
-                background: 'bg-gradient-warning',
-                shape: 'rounded-circle',
-              }"
-            />
-          </div>
+
 
         </div>
+
         <div class="row">
           <div class="col-lg-7 mb-lg">
             <!-- line chart -->
@@ -269,6 +330,7 @@ export default {
             <carousel />
           </div>
         </div>
+        <!--
         <div class="row mt-4">
           <div class="col-lg-7 mb-lg-0 mb-4">
             <div class="card">
@@ -351,6 +413,7 @@ export default {
             />
           </div>
         </div>
+        -->
       </div>
     </div>
   </div>
